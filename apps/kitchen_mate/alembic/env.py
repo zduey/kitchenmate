3a7bec92051e68d0b53e8da7498648fb5ff35f53
Zach Duey <zachduey@gmail.com>
2026-01-20 22:03:12 -0600

    fix alembic connection string (#14)

--- apps/kitchen_mate/alembic/env.py
+++ apps/kitchen_mate/alembic/env.py
@@ -1,18 +1,20 @@
 """Alembic environment configuration for async SQLAlchemy."""
 
+import os
 import asyncio
 from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy.ext.asyncio import create_async_engine, async_engine_from_config
 
 # Import models for autogenerate support
 from kitchen_mate.database.models import Base
 
 config = context.config
 target_metadata = Base.metadata
+db_url = f"sqlite+aiosqlite:///{os.getenv('CACHE_DB_PATH')}"
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
@@ -23,7 +25,7 @@ def run_migrations_offline() -> None:
 
     This configures the context with just a URL and not an Engine.
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.set_main_option("sqlalchemy.url", db_url)
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -45,11 +47,7 @@ def do_run_migrations(connection: Connection) -> None:
 
 async def run_async_migrations() -> None:
     """Run migrations in async mode."""
-    connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = create_async_engine(db_url, poolclass=pool.NullPool)
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
